c9f0e2788c885d8028450420924efcbfe6ff4cfe
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-06-09 17:15:12 +0200

    Add tmdb interfaces

--- src/types/interfaces/Tmdb.ts
+++ src/types/interfaces/Tmdb.ts
@@ -13,8 +13,98 @@ export interface Configuration {
 }
 
 export interface Show {
+  adult: boolean;
+  backdrop_path: string;
+  created_by: CreatedBy[];
+  episode_run_time: number[];
+  first_air_date: string;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  in_production: boolean;
+  languages: string[];
+  last_air_date: string;
+  last_episode_to_air: Episode;
+  name: string;
+  networks: Network[];
+  next_episode_to_air: null;
   number_of_episodes: number;
+  number_of_seasons: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: string;
   poster_path: string;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
+  seasons: Season[];
+  spoken_languages: SpokenLanguage[];
+  status: 'Ended';
+  tagline: string;
+  type: 'scripted';
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface Episode {
+  air_date: string;
+  episode_number: string;
+  id: number;
   name: string;
   overview: string;
+  production_code: string;
+  runtime: number;
+  season_number: number;
+  still_path: string;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface Network {
+  id: number;
+  logo_path: string;
+  name: string;
+  origin_country: string;
+}
+
+export interface ProductionCompany {
+  id: number;
+  logo_path: string;
+  name: string;
+  origin_country: string;
+}
+
+export interface ProductionCountry {
+  iso_3166_1: string;
+  name: string;
+}
+
+export interface Season {
+  air_date: string;
+  episode_count: number;
+  id: number;
+  name: string;
+  overview: string;
+  poster_path: string;
+  season_number: number;
+}
+
+export interface SpokenLanguage {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+}
+
+export interface Genre {
+  id: number;
+  name: string;
+}
+
+export interface CreatedBy {
+  credit_id: string;
+  gender: number;
+  id: number;
+  name: string;
+  profile_path: string;
 }

06766608fd38b03a0e0fd20529b69539cd14f645
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-06-14 16:16:13 +0200

    Clean up oauth config

--- src/app/auth-config.ts
+++ src/app/auth-config.ts
@@ -2,24 +2,12 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 import { Config } from './config';
 
 export const authCodeFlowConfig: AuthConfig = {
-  // Url of the Identity Provider
-  issuer: 'https://api.trakt.tv',
-
   loginUrl: 'https://api.trakt.tv/oauth/authorize',
-
   tokenEndpoint: 'https://api.trakt.tv/oauth/token',
-
-  // URL of the SPA to redirect the user to after login
   redirectUri: window.location.origin + '/redirect',
-
-  // The SPA's id. The SPA is registered with this id at the auth-server
-  // clientId: 'server.code',
   clientId: Config.traktClientId,
-
   responseType: 'code',
-
   scope: '',
   oidc: false,
-
   showDebugInformation: true,
 };

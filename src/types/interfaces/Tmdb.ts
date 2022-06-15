3522dd7f8043d639fe9918f705c4bf7ace465b97
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-06-15 22:16:44 +0200

    Don't show image if there is none

--- src/types/interfaces/Tmdb.ts
+++ src/types/interfaces/Tmdb.ts
@@ -56,7 +56,7 @@ export interface Episode {
   production_code: string;
   runtime: number;
   season_number: number;
-  still_path: string;
+  still_path?: string;
   vote_average: number;
   vote_count: number;
 }

2870af0c6b252bb9da11b867b4f27e6353d28d8e
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-06-20 12:56:36 +0200

    Add episode name to episode page and refactor mat-list-item

--- src/app/components/pages/episode/episode/episode.component.ts
+++ src/app/components/pages/episode/episode/episode.component.ts
@@ -2,7 +2,8 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { ShowService } from '../../../../services/show.service';
-import { EpisodeProgress } from '../../../../../types/interfaces/Trakt';
+import { Episode, EpisodeProgress } from '../../../../../types/interfaces/Trakt';
+import { SyncService } from '../../../../services/sync.service';
 
 @Component({
   selector: 'app-episode',
@@ -11,14 +12,19 @@ import { EpisodeProgress } from '../../../../../types/interfaces/Trakt';
 })
 export class EpisodeComponent implements OnInit, OnDestroy {
   subscriptions: Subscription[] = [];
-  episode?: EpisodeProgress;
+  episodeProgress?: EpisodeProgress;
+  episode?: Episode;
   slug?: string;
 
-  constructor(private route: ActivatedRoute, public showService: ShowService) {}
+  constructor(
+    private route: ActivatedRoute,
+    public showService: ShowService,
+    private syncService: SyncService
+  ) {}
 
   ngOnInit(): void {
     this.subscriptions = [
-      this.route.params.subscribe((params) => {
+      this.route.params.subscribe(async (params) => {
         this.slug = params['slug'];
         if (!this.slug) return;
 
@@ -28,11 +34,16 @@ export class EpisodeComponent implements OnInit, OnDestroy {
         const ids = this.showService.getIdForSlug(this.slug);
         if (!ids) return;
 
-        this.episode = this.showService.getEpisodeProgressLocally(
+        this.episodeProgress = this.showService.getEpisodeProgressLocally(
           ids.trakt,
           seasonNumber,
           episodeNumber
         );
+        if (!this.episodeProgress) return;
+
+        this.episode = undefined;
+        await this.syncService.syncShowsEpisodes(ids.trakt, seasonNumber, episodeNumber);
+        this.episode = this.showService.getEpisodeLocally(ids.trakt, seasonNumber, episodeNumber);
       }),
     ];
   }

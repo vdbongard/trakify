c2286b60da2569a9d1f0d564af4dd7ac06b1d792
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-07-12 20:31:41 +0200

    Refactor shows page to use base component

--- src/app/components/pages/shows/shows/shows.component.ts
+++ src/app/components/pages/shows/shows/shows.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subject, Subscription, tap } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Subject, takeUntil, tap } from 'rxjs';
 import { ShowService } from '../../../../services/show.service';
 import { TmdbService } from '../../../../services/tmdb.service';
 import { wait } from '../../../../helper/wait';
 import { ShowInfo } from '../../../../../types/interfaces/Show';
 import { DialogService } from '../../../../services/dialog.service';
+import { BaseComponent } from '../../../../helper/base-component';
 
 @Component({
   selector: 'app-shows-page',
   templateUrl: './shows.component.html',
   styleUrls: ['./shows.component.scss'],
 })
-export class ShowsComponent implements OnInit, OnDestroy {
-  subscriptions: Subscription[] = [];
+export class ShowsComponent extends BaseComponent implements OnInit {
   shows: ShowInfo[] = [];
   isLoading = new Subject<boolean>();
 
@@ -20,25 +20,24 @@ export class ShowsComponent implements OnInit, OnDestroy {
     public showService: ShowService,
     public tmdbService: TmdbService,
     public dialogService: DialogService
-  ) {}
-
-  ngOnInit(): void {
-    this.subscriptions = [
-      this.showService
-        .getShowsFilteredAndSorted$()
-        .pipe(tap(() => this.isLoading.next(true)))
-        .subscribe({
-          next: async (shows: ShowInfo[]) => {
-            this.shows = shows;
-            await wait();
-            this.isLoading.next(false);
-          },
-          error: () => this.isLoading.next(false),
-        }),
-    ];
+  ) {
+    super();
   }
 
-  ngOnDestroy(): void {
-    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+  ngOnInit(): void {
+    this.showService
+      .getShowsFilteredAndSorted$()
+      .pipe(
+        tap(() => this.isLoading.next(true)),
+        takeUntil(this.destroy$)
+      )
+      .subscribe({
+        next: async (shows: ShowInfo[]) => {
+          this.shows = shows;
+          await wait();
+          this.isLoading.next(false);
+        },
+        error: () => this.isLoading.next(false),
+      });
   }
 }

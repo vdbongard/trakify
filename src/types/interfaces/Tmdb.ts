ba7ec594f4e13243e3b224c21b423e7dec4e5396
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-07-18 14:51:34 +0200

    Add string to show status type

--- src/types/interfaces/Tmdb.ts
+++ src/types/interfaces/Tmdb.ts
@@ -40,7 +40,7 @@ export interface TmdbShow {
   production_countries: ProductionCountry[];
   seasons: Season[];
   spoken_languages: SpokenLanguage[];
-  status: 'Ended' | 'Returning Series' | 'Canceled';
+  status: 'Ended' | 'Returning Series' | 'Canceled' | 'In production';
   tagline: string;
   type: 'scripted';
   vote_average: number;

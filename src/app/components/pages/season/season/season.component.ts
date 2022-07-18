6e6e2e9307f50df7abad8db1d0d76ba8a45361cc
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-07-18 16:38:45 +0200

    Add breadcrumb to season page

--- src/app/components/pages/season/season/season.component.ts
+++ src/app/components/pages/season/season/season.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { ShowService } from '../../../../services/show.service';
 import { BaseComponent } from '../../../../helper/base-component';
 import { SeasonInfo } from '../../../../../types/interfaces/Show';
+import { BreadcrumbPart } from '../../../../shared/components/breadcrumb/breadcrumb.component';
 
 @Component({
   selector: 'app-season',
@@ -12,6 +13,7 @@ import { SeasonInfo } from '../../../../../types/interfaces/Show';
 })
 export class SeasonComponent extends BaseComponent implements OnInit {
   seasonInfo?: SeasonInfo;
+  breadcrumbParts?: BreadcrumbPart[];
 
   constructor(private route: ActivatedRoute, public showService: ShowService) {
     super();
@@ -25,6 +27,18 @@ export class SeasonComponent extends BaseComponent implements OnInit {
         }),
         takeUntil(this.destroy$)
       )
-      .subscribe(async (seasonInfo) => (this.seasonInfo = seasonInfo));
+      .subscribe(async (seasonInfo) => {
+        this.seasonInfo = seasonInfo;
+        this.breadcrumbParts = [
+          {
+            name: seasonInfo?.showTranslation?.title || seasonInfo?.show?.title,
+            link: `/series/s/${seasonInfo?.show?.ids.slug}`,
+          },
+          {
+            name: `Season ${seasonInfo?.seasonNumber}`,
+            link: `/series/s/${seasonInfo?.show?.ids.slug}/season/${seasonInfo?.seasonNumber}`,
+          },
+        ];
+      });
   }
 }

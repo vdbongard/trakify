cff0e10971d0490f50a872cf92321966b3a29e2b
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-07-30 21:35:55 +0200

    Move getting show info to component and update the page faster

--- src/app/components/pages/show/show/show.component.ts
+++ src/app/components/pages/show/show/show.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { BehaviorSubject, switchMap, takeUntil } from 'rxjs';
+import {
+  BehaviorSubject,
+  catchError,
+  combineLatest,
+  Observable,
+  of,
+  switchMap,
+  takeUntil,
+} from 'rxjs';
 import { TmdbService } from '../../../../services/tmdb.service';
 import { ShowService } from '../../../../services/trakt/show.service';
 import { SyncService } from '../../../../services/sync.service';
@@ -11,6 +19,13 @@ import { InfoService } from '../../../../services/info.service';
 import { LoadingState } from '../../../../../types/enum';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { onError } from '../../../../helper/error';
+import {
+  EpisodeFull,
+  ShowProgress,
+  ShowWatched,
+  TraktShow,
+} from '../../../../../types/interfaces/Trakt';
+import { TmdbEpisode, TmdbShow } from '../../../../../types/interfaces/Tmdb';
 
 @Component({
   selector: 'app-show',
@@ -41,15 +56,41 @@ export class ShowComponent extends BaseComponent implements OnInit {
       .pipe(
         switchMap((params) => {
           this.params = params;
-          return this.infoService.getShowInfo$(params['slug']);
+          if (!params['slug']) throw Error('Slug is empty');
+          return this.showService.getIdsBySlug$(params['slug'], true);
+        }),
+        switchMap((ids) => this.showService.getShow$(ids, false, true)),
+        switchMap((show) => {
+          if (!show) throw Error('Show is empty');
+          this.showInfo = { show };
+          this.loadingState.next(LoadingState.SUCCESS);
+          return combineLatest([
+            this.showService.getShowWatched$(show.ids.trakt).pipe(catchError(() => of(undefined))),
+            this.showService.getShowProgress$(show.ids.trakt),
+            this.tmdbService
+              .getTmdbShow$(show.ids, false, true)
+              .pipe(catchError(() => of(undefined))),
+            of(show),
+          ]);
+        }),
+        switchMap(([showWatched, showProgress, tmdbShow, show]) => {
+          this.showInfo = {
+            ...this.showInfo,
+            showWatched,
+            tmdbShow,
+            showProgress: showProgress
+              ? { ...showProgress, seasons: [...showProgress.seasons] }
+              : undefined,
+          };
+          return this.getNextEpisode(tmdbShow, showProgress, showWatched, show);
+        }),
+        switchMap(([nextEpisode, tmdbNextEpisode]) => {
+          this.showInfo = { ...this.showInfo, nextEpisode, tmdbNextEpisode };
+          return of(undefined);
         }),
         takeUntil(this.destroy$)
       )
       .subscribe({
-        next: (showInfo) => {
-          this.showInfo = showInfo;
-          this.loadingState.next(LoadingState.SUCCESS);
-        },
         error: (error) => onError(error, this.snackBar, this.loadingState),
       });
 
@@ -61,6 +102,52 @@ export class ShowComponent extends BaseComponent implements OnInit {
       },
       error: (error) => onError(error, this.snackBar, this.loadingState),
     });
+  }
+
+  private getNextEpisode(
+    tmdbShow: TmdbShow | undefined,
+    showProgress: ShowProgress | undefined,
+    showWatched: ShowWatched | undefined,
+    show: TraktShow
+  ): Observable<[EpisodeFull | null | undefined, TmdbEpisode | null | undefined]> {
+    const isShowEnded = tmdbShow ? ['Ended', 'Canceled'].includes(tmdbShow.status) : false;
+
+    const seasonNumber: number | null | undefined =
+      showProgress && showProgress?.next_episode !== null
+        ? showProgress?.next_episode?.season
+        : isShowEnded && showWatched
+        ? null
+        : 1;
+    const episodeNumber: number | null | undefined =
+      showProgress && showProgress?.next_episode !== null
+        ? showProgress?.next_episode?.number
+        : isShowEnded && showWatched
+        ? null
+        : 1;
+
+    const areNextEpisodesNumbersSet =
+      seasonNumber !== undefined &&
+      episodeNumber !== undefined &&
+      seasonNumber !== null &&
+      episodeNumber !== null;
+
+    if (areNextEpisodesNumbersSet) {
+      this.showInfo = {
+        ...this.showInfo,
+        nextEpisodeProgress: showProgress?.seasons
+          .find((season) => season.number === seasonNumber)
+          ?.episodes?.find((episode) => episode.number === episodeNumber),
+      };
+    }
+
+    return combineLatest([
+      areNextEpisodesNumbersSet
+        ? this.episodeService.getEpisode$(show.ids, seasonNumber, episodeNumber, false, true)
+        : of(seasonNumber as undefined | null),
+      areNextEpisodesNumbersSet
+        ? this.tmdbService.getTmdbEpisode$(show.ids.tmdb, seasonNumber, episodeNumber, false, true)
+        : of(seasonNumber as undefined | null),
+    ]);
   }
 
   addToHistory(showInfo: ShowInfo): void {

34fe6b15f4b1b2400de72a53d451a27459f31f18
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-08-03 20:31:05 +0200

    wip 2

--- src/app/shared/directives/transition-group.directive.ts
+++ src/app/shared/directives/transition-group.directive.ts
@@ -1,29 +1,45 @@
-import { AfterContentInit, ContentChildren, Directive, Input, QueryList } from '@angular/core';
+import { AfterViewInit, ContentChildren, Directive, Input, QueryList } from '@angular/core';
 import { TransitionGroupItemDirective } from './transition-group-item.directive';
 
 @Directive({
   selector: '[appTransitionGroup]',
 })
-export class TransitionGroupDirective implements AfterContentInit {
+export class TransitionGroupDirective implements AfterViewInit {
   @Input('appTransitionGroup') class?: string;
 
   @ContentChildren(TransitionGroupItemDirective) items?: QueryList<TransitionGroupItemDirective>;
 
-  ngAfterContentInit(): void {
-    this.refreshPosition('prevPos');
-    this.items?.changes.subscribe((items: TransitionGroupItemDirective[]) => {
-      items.forEach((item) => {
-        item.prevPos = item.newPos || item.prevPos;
-      });
+  ngAfterViewInit(): void {
+    setTimeout(() => this.refreshPosition('prevPos'), 0); // save init positions on next 'tick'
 
+    this.items?.changes.subscribe((items: TransitionGroupItemDirective[]) => {
+      items.forEach((item) => (item.prevPos = item.newPos || item.prevPos));
       items.forEach(this.runCallback);
       this.refreshPosition('newPos');
-      items.forEach(this.applyTranslation);
+      items.forEach((item) => (item.prevPos = item.prevPos || item.newPos)); // for new items
 
-      // force reflow to put everything in position
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const offSet = document.body.offsetHeight;
-      this.items?.forEach(this.runTransition.bind(this));
+      const animate = (): void => {
+        items.forEach(this.applyTranslation);
+        // @ts-ignore
+        this['_forceReflow'] = document.body.offsetHeight; // force reflow to put everything in position
+        this.items?.forEach(this.runTransition.bind(this));
+      };
+
+      const willMoveSome = items.some((item) => {
+        const dx = item.prevPos.left - item.newPos.left;
+        const dy = item.prevPos.top - item.newPos.top;
+        return dx || dy;
+      });
+
+      if (willMoveSome) {
+        animate();
+      } else {
+        setTimeout(() => {
+          // for removed items
+          this.refreshPosition('newPos');
+          animate();
+        }, 0);
+      }
     });
   }
 

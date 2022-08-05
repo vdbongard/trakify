f3917fa0f6fabe4513bf9eb21cea8ebac557103d
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-08-05 22:05:51 +0200

    Fix mark as seen on show page flicker to old episode in between

--- src/app/services/sync.service.ts
+++ src/app/services/sync.service.ts
@@ -464,7 +464,6 @@ export class SyncService {
           return;
         }
 
-        await this.syncNew();
         this.infoService.removeNewShow(ids.trakt);
       },
       error: (error) => onError(error, this.snackBar),
@@ -480,7 +479,6 @@ export class SyncService {
           return;
         }
 
-        await this.syncNew();
         this.infoService.addNewShow(ids, episode);
       },
       error: (error) => onError(error, this.snackBar),

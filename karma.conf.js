ab47e5f27347f054b7f98b9e45cd8a73ba2a69db
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-08-11 14:43:36 +0200

    Replace series-tracker with trakify

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/series-tracker'),
+      dir: require('path').join(__dirname, './coverage/trakify'),
       subdir: '.',
       reporters: [{ type: 'html' }, { type: 'text-summary' }],
     },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,7 +21,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'series-tracker app is running!'
+      'trakify app is running!'
     );
   });
 });

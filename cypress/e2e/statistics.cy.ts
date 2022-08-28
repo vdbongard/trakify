86927d9f5e3ec5b81fa11ccfa7000a6b91b11a60
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-08-28 15:55:13 +0200

    Add e2e test for empty statistics

---
+++ cypress/e2e/statistics.cy.ts
@@ -0,0 +1,12 @@
+describe('Statistics', () => {
+  beforeEach(() => {
+    cy.login();
+    cy.visit('/statistics');
+  });
+
+  it('should show empty statistics', () => {
+    cy.contains('0 / 0');
+    cy.contains('0 shows running');
+    cy.contains('0 shows with next episode');
+  });
+});

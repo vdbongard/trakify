700c340be65ecc6f9c1122c5d58bb977c41d7c69
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-09-02 14:26:13 +0200

    Disable hash routes for now, login redirect does not work with it, Lighthouse will fail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -30,7 +30,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled', useHash: true })],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled', useHash: false })],
   exports: [RouterModule],
   providers: [CanActivateLoggedIn],
 })

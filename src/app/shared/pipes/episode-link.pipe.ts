7fcb5c6b5526ec7c90e9e565b44a6569023e45a8
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-09-05 16:10:17 +0200

    Fix episode undefined error

--- src/app/shared/pipes/episode-link.pipe.ts
+++ src/app/shared/pipes/episode-link.pipe.ts
@@ -5,8 +5,8 @@ import { Episode, Show } from '../../../types/interfaces/Trakt';
   name: 'episodeLink',
 })
 export class EpisodeLinkPipe implements PipeTransform {
-  transform(episode: Episode | undefined, show: Show): string {
-    if (!episode) throw Error('Episode is empty');
+  transform(episode: Episode | undefined, show: Show): string | null {
+    if (!episode) return null;
     return `/series/s/${show.ids.slug}/season/${episode.season}/episode/${episode.number}`;
   }
 }

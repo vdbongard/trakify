5fcb1ccbefac0e8c629f4a594008ad65a0aef317
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-09-11 21:50:22 +0200

    Add e2e tests for episode

--- cypress/e2e/episode.cy.ts
+++ cypress/e2e/episode.cy.ts
@@ -1,5 +1,17 @@
 describe('Episode', () => {
-  it('should show the episode', () => {});
+  beforeEach(() => {
+    cy.login();
+    cy.visit('http://localhost:4200/series/s/game-of-thrones/season/1/episode/1?sync=0');
+  });
+
+  it('should show the episode', () => {
+    cy.contains('Game of Thrones / Season 1 / Episode 1').should('exist');
+    cy.contains('18. Apr. 2011 (Mon.)').should('exist');
+    cy.contains('S01E01 Winter Is Coming').should('exist');
+    cy.contains('Mark as seen').should('not.be.disabled');
+    cy.contains('Jon Arryn, the').should('exist');
+    cy.contains('Source: TMDB').should('exist');
+  });
 
   it('should jump to the next/previous episode', () => {});
 

e811d1c63fd22961c9449d5ad2abab7c3de1a33a
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-09-11 20:24:25 +0200

    Add e2e tests for sidebar

--- cypress/e2e/sidebar.cy.ts
+++ cypress/e2e/sidebar.cy.ts
@@ -1,9 +1,31 @@
 describe('Sidebar', () => {
-  it('should show the sidebar', () => {});
+  beforeEach(() => {
+    cy.login();
+    cy.visit('/');
+  });
 
-  it('should navigate to shows', () => {});
+  it('should show the sidebar', () => {
+    cy.get('.sidenav').should('exist');
+  });
 
-  it('should navigate to lists', () => {});
+  it('should navigate to shows', () => {
+    cy.get('.sidenav').within(() => {
+      cy.contains('Shows').click();
+      cy.url().should('equal', Cypress.config().baseUrl + 'series');
+    });
+  });
 
-  it('should navigate to statistics', () => {});
+  it('should navigate to lists', () => {
+    cy.get('.sidenav').within(() => {
+      cy.contains('Lists').click();
+      cy.url().should('equal', Cypress.config().baseUrl + 'lists');
+    });
+  });
+
+  it('should navigate to statistics', () => {
+    cy.get('.sidenav').within(() => {
+      cy.contains('Statistics').click();
+      cy.url().should('equal', Cypress.config().baseUrl + 'statistics');
+    });
+  });
 });

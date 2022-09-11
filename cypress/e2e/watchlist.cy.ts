3515e5d1fdaea6a7e06f04b1f75f8482bc542c8b
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-09-11 15:48:42 +0200

    Add e2e test for watchlist

--- cypress/e2e/watchlist.cy.ts
+++ cypress/e2e/watchlist.cy.ts
@@ -8,5 +8,18 @@ describe('Watchlist', () => {
     cy.contains('No shows in the list.');
   });
 
-  it('should show watchlist shows', () => {});
+  it('should add/remove show from watchlist', () => {
+    // add
+    cy.visit('/series/add-series?search=Game%20of%20Thrones');
+    cy.get('[data-test-id="show"]:first [data-test-id="add-button"]').should('exist').click();
+    cy.contains('Added show to the watchlist');
+    cy.get('[data-test-id="show"]:first [data-test-id="remove-button"]').should('exist');
+    cy.visit('/series/watchlist');
+    cy.get('[data-test-id="show"]').should('have.length', 1);
+
+    // remove
+    cy.get('[data-test-id="show-item-menu"]').click();
+    cy.contains('Remove from watchlist').click();
+    cy.get('[data-test-id="show"]').should('not.exist');
+  });
 });

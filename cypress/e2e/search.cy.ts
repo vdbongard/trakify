44072445bf8cb8f705ccfab66440d0d91e8c63a6
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-09-12 16:41:29 +0200

    Add e2e tests for search

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -11,7 +11,28 @@ describe('Search', () => {
     cy.get(e.showItem).should('not.exist');
   });
 
-  it('should search for a show', () => {});
+  it('should focus the search input on load', () => {
+    cy.get(e.searchInput).should('have.focus');
+  });
 
-  it('should search with no result', () => {});
+  it('should search for a show', () => {
+    cy.intercept('GET', ' https://api.trakt.tv/sync/watched/shows?extended=noseasons').as(
+      'getShowsWatched'
+    );
+
+    cy.visit('/series/s/game-of-thrones?sync=0');
+    cy.contains('Mark as seen').click();
+    cy.wait('@getShowsWatched');
+
+    cy.visit('/series/search?sync=0');
+    cy.get(e.searchInput).type('g');
+    cy.get(e.showItem).should('have.length', 1);
+
+    cy.removeWatchedShows();
+  });
+
+  it('should search with no result', () => {
+    cy.get(e.searchInput).type('a');
+    cy.get(e.showItem).should('not.exist');
+  });
 });

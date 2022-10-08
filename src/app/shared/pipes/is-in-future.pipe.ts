a20d531534ea20623ef671f7cf47a5a819f39b4d
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-10-08 20:52:28 +0200

    Don't show mark as seen buttons when first episode is in future

--- src/app/shared/pipes/is-in-future.pipe.ts
+++ src/app/shared/pipes/is-in-future.pipe.ts
@@ -5,7 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class IsInFuturePipe implements PipeTransform {
   transform(dateString: string | null | undefined): boolean {
-    if (!dateString) return false;
+    if (dateString === undefined) return false;
+    if (dateString === null) return true;
     return new Date(dateString) > new Date();
   }
 }

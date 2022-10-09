dec0b04681c23631c74b911c58f1506bf0b0c196
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-10-09 11:25:32 +0200

    Rename trackBy

--- src/app/shared/components/shows/shows.component.ts
+++ src/app/shared/components/shows/shows.component.ts
@@ -28,7 +28,7 @@ export class ShowsComponent {
 
   paths = Paths;
 
-  showId(index: number, showInfo: ShowInfo): string {
+  id(index: number, showInfo: ShowInfo): string {
     return '' + showInfo.show?.ids.trakt + showInfo.nextEpisode?.ids.trakt;
   }
 }

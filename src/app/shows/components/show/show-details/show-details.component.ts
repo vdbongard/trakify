299cc7add5bd7b392bc4d9762e7cc65492dc68c5
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-11-04 18:12:24 +0100

    Fix show details error

--- src/app/shows/components/show/show-details/show-details.component.ts
+++ src/app/shows/components/show/show-details/show-details.component.ts
@@ -9,17 +9,19 @@ import { GenrePipe } from '../../../../shared/pipes/genre.pipe';
   imports: [CommonModule, GenrePipe],
   template: `
     <div *ngIf="tmdbShow" class="details">
-      <p>Genre: {{ tmdbShow.genres | genre }}</p>
-      <p>Created by: {{ tmdbShow.created_by[0].name }}</p>
+      <p *ngIf="tmdbShow.genres">Genre: {{ tmdbShow.genres | genre }}</p>
+      <p *ngIf="tmdbShow.created_by[0]">Created by: {{ tmdbShow.created_by[0].name }}</p>
       <p *ngIf="tmdbShow.first_air_date">
         First aired: {{ tmdbShow.first_air_date | date: 'd. MMM. yyyy' }}
       </p>
-      <p>
+      <p *ngIf="tmdbShow.vote_count">
         Vote: {{ tmdbShow.vote_average }}
         <span class="votes">({{ tmdbShow.vote_count }} votes)</span>
       </p>
-      <p>Runtime: {{ tmdbShow.episode_run_time[0] + 'min' }}</p>
-      <p>Type: {{ tmdbShow.type }}</p>
+      <p *ngIf="tmdbShow.episode_run_time[0]">
+        Runtime: {{ tmdbShow.episode_run_time[0] + 'min' }}
+      </p>
+      <p *ngIf="tmdbShow.type !== 'Scripted'">Type: {{ tmdbShow.type }}</p>
       <p>
         <a [href]="tmdbShow.homepage" target="_blank" rel="noopener norefferer">
           {{ tmdbShow.homepage }}

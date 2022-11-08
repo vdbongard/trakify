cae5124a6bf48380ca594f65c63ddc9aad25b24f
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-11-08 23:25:15 +0100

    Add case when show is airing in the future instead of the past

--- src/app/shows/components/show/show-details/show-details.component.ts
+++ src/app/shows/components/show/show-details/show-details.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { TmdbShow } from '@type/interfaces/Tmdb';
 import { GenrePipe } from '../../../../shared/pipes/genre.pipe';
 import { CreatedByPipe } from '../../../pipes/createdBy.pipe';
+import { IsInFuturePipe } from '../../../../shared/pipes/is-in-future.pipe';
 
 @Component({
   selector: 't-show-details',
   standalone: true,
-  imports: [CommonModule, GenrePipe, CreatedByPipe],
+  imports: [CommonModule, GenrePipe, CreatedByPipe, IsInFuturePipe],
   template: `
     <h2 class="mat-subheading-2">Details</h2>
     <div class="details">
@@ -17,7 +18,11 @@ import { CreatedByPipe } from '../../../pipes/createdBy.pipe';
           Created by: {{ tmdbShow.created_by | createdBy }}
         </p>
         <p *ngIf="tmdbShow.first_air_date" class="mat-body">
-          First aired: {{ tmdbShow.first_air_date | date: 'd. MMM. yyyy' }}
+          <ng-container *ngIf="tmdbShow.first_air_date | isInFuture; else isInPast">
+            First airing
+          </ng-container>
+          <ng-template #isInPast>First aired</ng-template>
+          : {{ tmdbShow.first_air_date | date: 'd. MMM. yyyy' }}
         </p>
         <p *ngIf="tmdbShow.vote_count" class="mat-body">
           Rating: {{ tmdbShow.vote_average | number: '1.0-1' }}

eca7f29282ecf56e64a8e017b8d01ac72888b16c
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-11-08 23:19:39 +0100

    Add empty show details text and improve layout

--- src/app/shows/components/show/show-details/show-details.component.ts
+++ src/app/shows/components/show/show-details/show-details.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TmdbShow } from '@type/interfaces/Tmdb';
 import { GenrePipe } from '../../../../shared/pipes/genre.pipe';
@@ -10,20 +10,25 @@ import { CreatedByPipe } from '../../../pipes/createdBy.pipe';
   imports: [CommonModule, GenrePipe, CreatedByPipe],
   template: `
     <h2 class="mat-subheading-2">Details</h2>
-    <div *ngIf="tmdbShow" class="details">
-      <p *ngIf="tmdbShow.genres.length">Genre: {{ tmdbShow.genres | genre }}</p>
-      <p *ngIf="tmdbShow.created_by[0]">Created by: {{ tmdbShow.created_by | createdBy }}</p>
-      <p *ngIf="tmdbShow.first_air_date">
-        First aired: {{ tmdbShow.first_air_date | date: 'd. MMM. yyyy' }}
-      </p>
-      <p *ngIf="tmdbShow.vote_count">
-        Rating: {{ tmdbShow.vote_average | number: '1.0-1' }}
-        <span class="votes">({{ tmdbShow.vote_count }} votes)</span>
-      </p>
-      <p *ngIf="tmdbShow.episode_run_time[0]">
-        Runtime: {{ tmdbShow.episode_run_time[0] + 'min' }}
-      </p>
-      <p *ngIf="tmdbShow.type !== 'Scripted'">Type: {{ tmdbShow.type }}</p>
+    <div class="details">
+      <ng-container *ngIf="tmdbShow && hasDetails; else noDetails">
+        <p *ngIf="tmdbShow.genres.length" class="mat-body">Genre: {{ tmdbShow.genres | genre }}</p>
+        <p *ngIf="tmdbShow.created_by.length" class="mat-body">
+          Created by: {{ tmdbShow.created_by | createdBy }}
+        </p>
+        <p *ngIf="tmdbShow.first_air_date" class="mat-body">
+          First aired: {{ tmdbShow.first_air_date | date: 'd. MMM. yyyy' }}
+        </p>
+        <p *ngIf="tmdbShow.vote_count" class="mat-body">
+          Rating: {{ tmdbShow.vote_average | number: '1.0-1' }}
+          <span class="votes">({{ tmdbShow.vote_count }} votes)</span>
+        </p>
+        <p *ngIf="tmdbShow.episode_run_time.length" class="mat-body">
+          Runtime: {{ tmdbShow.episode_run_time[0] + 'min' }}
+        </p>
+        <p *ngIf="tmdbShow.type !== 'Scripted'" class="mat-body">Type: {{ tmdbShow.type }}</p>
+      </ng-container>
+      <ng-template #noDetails><p class="mat-body">No details available.</p></ng-template>
     </div>
   `,
   styles: [
@@ -33,15 +38,15 @@ import { CreatedByPipe } from '../../../pipes/createdBy.pipe';
       .details {
         display: grid;
         grid-template-columns: 1fr;
-        margin-top: rem(-8px);
+        gap: 0.5rem;
 
         @media (min-width: $breakpoint-sm) {
           grid-template-columns: 1fr 1fr;
+          gap: 1rem;
         }
 
         > * {
-          margin-top: 0.5rem;
-          margin-bottom: 0.5rem;
+          margin: 0;
         }
       }
 
@@ -52,6 +57,22 @@ import { CreatedByPipe } from '../../../pipes/createdBy.pipe';
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ShowDetailsComponent {
+export class ShowDetailsComponent implements OnChanges {
   @Input() tmdbShow?: TmdbShow | null;
+
+  hasDetails = false;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['tmdbShow']) {
+      this.hasDetails = Boolean(
+        this.tmdbShow &&
+          (this.tmdbShow.genres.length ||
+            this.tmdbShow.created_by.length ||
+            this.tmdbShow.first_air_date ||
+            this.tmdbShow.vote_count ||
+            this.tmdbShow.episode_run_time.length ||
+            this.tmdbShow.type !== 'Scripted')
+      );
+    }
+  }
 }

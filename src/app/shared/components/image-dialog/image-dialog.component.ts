ab46ba13c2c1a5352e8ecf81cca4c2d3f31ee497
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-11-14 22:33:25 +0100

    Close image dialog on image click

--- src/app/shared/components/image-dialog/image-dialog.component.ts
+++ src/app/shared/components/image-dialog/image-dialog.component.ts
@@ -1,13 +1,22 @@
 import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ImageDialogData } from '@type/interfaces/Dialog';
 
 @Component({
   selector: 't-image-dialog',
   standalone: true,
   imports: [CommonModule, NgOptimizedImage],
-  template: ` <img [src]="data.imageUrl" [alt]="data.name" class="image" fetchpriority="high" /> `,
+  template: `
+    <img
+      [src]="data.imageUrl"
+      [alt]="data.name"
+      class="image"
+      fetchpriority="high"
+      (click)="dialogRef.close()"
+      (keyup)="dialogRef.close()"
+    />
+  `,
   styles: [
     `
       .image {
@@ -19,5 +28,8 @@ import { ImageDialogData } from '@type/interfaces/Dialog';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ImageDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: ImageDialogData) {}
+  constructor(
+    public dialogRef: MatDialogRef<ImageDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: ImageDialogData
+  ) {}
 }

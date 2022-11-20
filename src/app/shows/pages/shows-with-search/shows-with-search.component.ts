fe09d44eff2c9216f5689b67d1831f447c0b4b60
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2022-11-20 21:58:08 +0100

    Migrate to mdc components

--- src/app/shows/pages/shows-with-search/shows-with-search.component.ts
+++ src/app/shows/pages/shows-with-search/shows-with-search.component.ts
@@ -221,8 +221,7 @@ export class ShowsWithSearchComponent extends Base implements OnInit, OnDestroy 
     });
   }
 
-  async changeShowsSelection(chip: Chip, chipRef: HTMLElement): Promise<void> {
-    chipRef.blur();
+  async changeShowsSelection(chip: Chip): Promise<void> {
     await this.router.navigate([], {
       queryParamsHandling: 'merge',
       queryParams: { slug: chip.slug },

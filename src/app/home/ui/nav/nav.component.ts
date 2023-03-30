027e651e9d18407f9c5c2bbd8b4e682a8b4d4ac8
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2023-03-30 13:50:44 +0200

    Add swiping gesture to change tabs

--- src/app/home/ui/nav/nav.component.ts
+++ src/app/home/ui/nav/nav.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, inject, Input, NgZone, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTabNav, MatTabsModule } from '@angular/material/tabs';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
@@ -23,7 +23,10 @@ import * as Paths from '@shared/paths';
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
 })
-export class NavComponent {
+export class NavComponent implements OnInit, OnDestroy {
+  ngZone = inject(NgZone);
+  router = inject(Router);
+
   @Input() isLoggedIn = false;
   @Input() isDesktop = true;
   @Input() activeTabLink?: Link;
@@ -38,6 +41,9 @@ export class NavComponent {
     { name: 'Statistics', url: Paths.statistics({}), icon: 'bar_chart' },
   ];
 
+  // todo general swipe directive
+  down: { x: number; y: number } | null = null;
+
   sidenavClosedStart(): void {
     (document.activeElement as HTMLElement | null)?.blur();
   }
@@ -46,4 +52,108 @@ export class NavComponent {
     (document.activeElement as HTMLElement | null)?.blur();
     this.tabs?._alignInkBarToSelectedTab();
   }
+
+  ngOnInit(): void {
+    // needed to have the same reference in the event listener
+    this.onDown = this.onDown.bind(this);
+    this.onUp = this.onUp.bind(this);
+
+    this.addDownListener();
+  }
+
+  ngOnDestroy(): void {
+    this.removeDownListener();
+    this.removeUpListener();
+  }
+
+  addDownListener(): void {
+    this.ngZone.runOutsideAngular(() => {
+      document.addEventListener('pointerdown', this.onDown, { passive: true });
+    });
+  }
+
+  removeDownListener(): void {
+    this.ngZone.runOutsideAngular(() => {
+      document.removeEventListener('pointerdown', this.onDown);
+    });
+  }
+
+  addUpListener(): void {
+    this.ngZone.runOutsideAngular(() => {
+      document.addEventListener('pointerup', this.onUp, { passive: true });
+      document.addEventListener('dragend', this.onUp, { passive: true });
+    });
+  }
+
+  removeUpListener(): void {
+    this.ngZone.runOutsideAngular(() => {
+      document.removeEventListener('pointerup', this.onUp);
+      document.removeEventListener('dragend', this.onUp);
+    });
+  }
+
+  onDown(event: PointerEvent): void {
+    this.addUpListener();
+    if (!this.activeTabLink) return;
+
+    this.down = { x: event.clientX, y: event.clientY };
+    console.log('down', event);
+  }
+
+  async onUp(event: PointerEvent | DragEvent): Promise<void> {
+    this.removeUpListener();
+    if (!this.activeTabLink) return;
+
+    const up = { x: event.clientX, y: event.clientY };
+
+    if (isSwipeRight(this.down, up)) {
+      this.down = null;
+
+      // todo call general callback
+      await this.ngZone.run(async () => {
+        console.log('swipe right');
+        if (this.tabs) {
+          const newLinkIndex = mod(this.tabs.selectedIndex - 1, this.tabLinks.length);
+          const link: Link | undefined = this.tabLinks[newLinkIndex];
+          if (!link) return;
+          await this.router.navigateByUrl(link.url);
+        }
+      });
+    } else if (isSwipeLeft(this.down, up)) {
+      this.down = null;
+
+      // todo call general callback
+      await this.ngZone.run(async () => {
+        console.log('swipe right');
+        if (this.tabs) {
+          const newLinkIndex = mod(this.tabs.selectedIndex + 1, this.tabLinks.length);
+          const link: Link | undefined = this.tabLinks[newLinkIndex];
+          if (!link) return;
+          await this.router.navigateByUrl(link.url);
+        }
+      });
+    }
+
+    console.log('up', event);
+  }
+}
+
+export function isSwipeRight(
+  down: { x: number; y: number } | null,
+  up: { x: number; y: number } | null
+): boolean {
+  if (!down || !up) return false;
+  return up.x - down.x > 50;
+}
+
+export function isSwipeLeft(
+  down: { x: number; y: number } | null,
+  up: { x: number; y: number } | null
+): boolean {
+  if (!down || !up) return false;
+  return up.x - down.x < -50;
+}
+
+export function mod(n: number, m: number): number {
+  return ((n % m) + m) % m;
 }

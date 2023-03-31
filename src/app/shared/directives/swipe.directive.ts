63ebecff9e4223593b62c7b447a2a123aff8b4e6
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2023-03-31 21:28:48 +0200

    Add min time to swipe gesture

--- src/app/shared/directives/swipe.directive.ts
+++ src/app/shared/directives/swipe.directive.ts
@@ -23,7 +23,14 @@ export class SwipeDirective implements OnInit, OnDestroy {
   @Output() swipeLeft = new EventEmitter<void>();
   @Output() swipeRight = new EventEmitter<void>();
 
-  down: { x: number; y: number } | null = null;
+  swipeThreshold = 20; // px
+  swipeTimeout = 500; // ms
+
+  xDown: number | null = null;
+  yDown: number | null = null;
+  xDiff: number | null = null;
+  yDiff: number | null = null;
+  timeDown: number | null = null;
 
   ngOnInit(): void {
     // needed to have the same reference in the event listener
@@ -36,6 +43,62 @@ export class SwipeDirective implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.removeDownListener();
     this.removeUpListener();
+  }
+
+  onDown(e: PointerEvent): void {
+    this.timeDown = Date.now();
+    this.xDown = e.clientX;
+    this.yDown = e.clientY;
+    this.xDiff = 0;
+    this.yDiff = 0;
+
+    this.addUpListener();
+  }
+
+  onUp(event: PointerEvent | DragEvent | TouchEvent): void {
+    this.removeUpListener();
+
+    const timeDiff = Date.now() - this.timeDown!;
+    let swipeDirection = '';
+    const up =
+      event instanceof TouchEvent
+        ? {
+            x: event.changedTouches[0].clientX,
+            y: event.changedTouches[0].clientY,
+          }
+        : { x: event.clientX, y: event.clientY };
+
+    this.xDiff = this.xDown! - up.x;
+    this.yDiff = this.yDown! - up.y;
+
+    if (Math.abs(this.xDiff) > Math.abs(this.yDiff)) {
+      if (Math.abs(this.xDiff) > this.swipeThreshold && timeDiff < this.swipeTimeout) {
+        if (this.xDiff > 0) {
+          swipeDirection = 'swiped-left';
+        } else {
+          swipeDirection = 'swiped-right';
+        }
+      }
+    } else if (Math.abs(this.yDiff) > this.swipeThreshold && timeDiff < this.swipeTimeout) {
+      if (this.yDiff > 0) {
+        swipeDirection = 'swiped-up';
+      } else {
+        swipeDirection = 'swiped-down';
+      }
+    }
+
+    if (swipeDirection !== '') {
+      if (swipeDirection === 'swiped-left') {
+        this.ngZone.run(() => this.swipeLeft.emit());
+      } else if (swipeDirection === 'swiped-right') {
+        this.ngZone.run(() => this.swipeRight.emit());
+      }
+    }
+
+    // reset values
+    this.xDown = null;
+    this.yDown = null;
+    this.timeDown = null;
   }
 
   addDownListener(): void {
@@ -64,48 +127,5 @@ export class SwipeDirective implements OnInit, OnDestroy {
       this.document.removeEventListener('touchend', this.onUp);
       this.document.removeEventListener('dragend', this.onUp);
     });
-  }
-
-  onDown(event: PointerEvent): void {
-    if (this.swipeDisabled) return;
-
-    this.addUpListener();
-
-    this.down = { x: event.clientX, y: event.clientY };
-  }
-
-  onUp(event: PointerEvent | DragEvent | TouchEvent): void {
-    this.removeUpListener();
-
-    if (this.swipeDisabled) return;
-
-    const up =
-      event instanceof TouchEvent
-        ? {
-            x: event.changedTouches[0].clientX,
-            y: event.changedTouches[0].clientY,
-          }
-        : { x: event.clientX, y: event.clientY };
-
-    if (this.isSwipeLeft(this.down, up)) {
-      this.ngZone.run(() => this.swipeLeft.emit());
-    } else if (this.isSwipeRight(this.down, up)) {
-      this.ngZone.run(() => this.swipeRight.emit());
-    }
-
-    this.down = null;
-  }
-
-  isSwipeLeft(down: { x: number; y: number } | null, up: { x: number; y: number } | null): boolean {
-    if (!down || !up) return false;
-    return up.x - down.x < -50;
-  }
-
-  isSwipeRight(
-    down: { x: number; y: number } | null,
-    up: { x: number; y: number } | null
-  ): boolean {
-    if (!down || !up) return false;
-    return up.x - down.x > 50;
   }
 }

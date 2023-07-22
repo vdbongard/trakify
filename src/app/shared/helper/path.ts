10aefe711c8fe0f4a05271895d857e410d31e570
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2023-07-22 12:09:39 +0200

    refactor: check for Path in the path function

--- src/app/shared/helper/path.ts
+++ src/app/shared/helper/path.ts
@@ -1,3 +1,16 @@
-export function path(routePath: string, prefixToRemove = ''): string {
+import type { Path } from 'static-path';
+
+export function path<T extends string, U extends string>(
+  routePath: string | Path<T>,
+  prefixToRemove: string | Path<U> = '',
+): string {
+  if (typeof routePath === 'function' && 'pattern' in routePath) {
+    routePath = routePath.pattern;
+  }
+
+  if (typeof prefixToRemove === 'function' && 'pattern' in prefixToRemove) {
+    prefixToRemove = prefixToRemove.pattern;
+  }
+
   return routePath.replace(prefixToRemove + '/', '');
 }

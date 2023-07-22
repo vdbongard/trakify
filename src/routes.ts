648971a2bd3bd47349089dc9b197696be980e2ee
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2023-07-22 12:11:06 +0200

    feat: disable login-new route for now

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,15 @@
 import { Routes } from '@angular/router';
 import { path } from '@helper/path';
-import { about, lists, login, loginNew, redirect, shows, statistics } from '@shared/paths';
+import { about, lists, login, redirect, shows, statistics } from '@shared/paths';
 import { LoginComponent } from './app/home/pages/login/login.component';
 import { RedirectComponent } from './app/home/pages/redirect/redirect.component';
 import { loggedIn } from '@shared/canActivate/logged-in';
 import { ErrorComponent } from './app/home/pages/error/error.component';
-import { LoginNewComponent } from './app/home/pages/login-new/login-new.component';
 import { AboutComponent } from './app/home/pages/about/about.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: path(shows), pathMatch: 'full' },
   { path: path(login), component: LoginComponent, title: 'Login - Trakify' },
-  { path: path(loginNew), component: LoginNewComponent, title: 'Login - Trakify' },
   { path: path(redirect), component: RedirectComponent },
   {
     path: path(shows),

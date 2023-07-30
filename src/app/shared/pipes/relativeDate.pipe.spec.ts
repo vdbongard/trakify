7e8bc7cac3b23a3e0644005cf10fcc70cee6c018
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2023-07-30 16:49:57 +0200

    refactor: use signal for relative date

--- src/app/shared/pipes/relativeDate.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RelativeDatePipe } from './relativeDate.pipe';
-
-describe('RelativePipe', () => {
-  it('create an instance', () => {
-    const pipe = new RelativeDatePipe();
-    expect(pipe).toBeTruthy();
-  });
-});

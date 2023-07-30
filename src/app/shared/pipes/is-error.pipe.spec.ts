ca90eb22a9432ab8505b0162f8fd6d160c76c53f
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2023-07-30 14:31:54 +0200

    refactor: use signal for show page error

--- src/app/shared/pipes/is-error.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { IsErrorPipe } from './is-error.pipe';
-
-describe('IsErrorPipe', () => {
-  it('create an instance', () => {
-    const pipe = new IsErrorPipe();
-    expect(pipe).toBeTruthy();
-  });
-});

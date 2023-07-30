60473fca5534a603da1c91dcbf7a1a5637762c31
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2023-07-30 16:38:04 +0200

    refactor: use signal for season progress

--- src/app/pages/shows/pages/show/ui/show-season-item/show-season-item.component.ts
+++ src/app/pages/shows/pages/show/ui/show-season-item/show-season-item.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnChanges } from '@angular/core';
-import type { EpisodeFull, SeasonProgress } from '@type/Trakt';
+import { Component, computed, Input, OnChanges, Signal } from '@angular/core';
+import type { EpisodeFull, ShowProgress } from '@type/Trakt';
 import type { TmdbShowSeason } from '@type/Tmdb';
 import { SeasonTitlePipe } from '../../../../utils/pipes/season-title.pipe';
 import { CommonModule } from '@angular/common';
@@ -15,19 +15,25 @@ import { getAiredEpisodesInSeason } from '@helper/episodes';
   imports: [CommonModule, SeasonTitlePipe, MatProgressBarModule],
 })
 export class ShowSeasonItemComponent implements OnChanges {
-  @Input() seasonProgress?: SeasonProgress;
+  @Input({ required: true }) showProgress!: Signal<ShowProgress | undefined>;
   @Input() seasonsEpisodes?: Record<string, EpisodeFull[] | undefined> | null;
   @Input() season?: TmdbShowSeason;
+
+  seasonProgress = computed(() => {
+    const showProgress = this.showProgress();
+    if (!showProgress) return;
+    return showProgress.seasons.find((season) => season.number === this.season?.season_number);
+  });
 
   episodesAired = 0;
 
   ngOnChanges(changes: SimpleChangesTyped<this>): void {
     if (changes.seasonProgress || changes.seasonsEpisodes) {
-      const seasonNumber = this.seasonProgress?.number ?? this.season?.season_number;
+      const seasonNumber = this.seasonProgress()?.number ?? this.season?.season_number;
       if (seasonNumber === undefined) return;
       this.episodesAired = getAiredEpisodesInSeason(
         this.seasonsEpisodes?.[seasonNumber] ?? [],
-        this.seasonProgress,
+        this.seasonProgress(),
       );
     }
   }
--- src/app/shared/pipes/progress-by-tmdb-season.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SeasonProgressBySeasonNumberPipe } from './progress-by-tmdb-season.pipe';
-
-describe('ProgressByTmdbSeasonPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SeasonProgressBySeasonNumberPipe();
-    expect(pipe).toBeTruthy();
-  });
-});

50ec233fd27c10f5ca1f4eb75940d1f26c638596
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2023-07-30 14:13:50 +0200

    refactor: use signal for show createdBy

--- src/app/pages/shows/utils/pipes/createdBy.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CreatedByPipe } from './createdBy.pipe';
-
-describe('CreatorPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CreatedByPipe();
-    expect(pipe).toBeTruthy();
-  });
-});

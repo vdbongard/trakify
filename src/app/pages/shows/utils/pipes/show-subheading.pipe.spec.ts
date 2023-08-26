ce13c25fec3db4978fbe3b8eb3da6c1c23036179
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2023-08-26 12:12:38 +0200

    refactor: use signal for show subheading

--- src/app/pages/shows/utils/pipes/show-subheading.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ShowSubheadingPipe } from './show-subheading.pipe';
-
-describe('ShowSubheadingPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ShowSubheadingPipe();
-    expect(pipe).toBeTruthy();
-  });
-});

bb3e00aab0aa7a8896784b75b5c0be433d79283e
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2023-10-14 19:03:19 +0200

    refactor: use computed instead of combineLatest

--- src/app/pages/statistics/data/stats.service.ts
+++ src/app/pages/statistics/data/stats.service.ts
@@ -1,6 +1,6 @@
-import { inject, Injectable, Injector } from '@angular/core';
+import { computed, inject, Injectable, Injector, Signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { combineLatest, map, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ShowService } from '../../shows/data/show.service';
 import { EpisodeService } from '../../shows/data/episode.service';
 import { TmdbService } from '../../shows/data/tmdb.service';
@@ -13,7 +13,6 @@ import { parseResponse } from '@operator/parseResponse';
 import { urlReplace } from '@helper/urlReplace';
 import { API } from '@shared/api';
 import { isShowEnded } from '@helper/isShowEnded';
-import { toObservable } from '@angular/core/rxjs-interop';
 
 @Injectable({
   providedIn: 'root',
@@ -29,95 +28,91 @@ export class StatsService {
     return this.http.get<Stats>(urlReplace(API.stats, [userId])).pipe(parseResponse(statsSchema));
   }
 
-  getStats$(): Observable<[ShowStats, EpisodeStats]> {
-    const showStats: Observable<ShowStats> = combineLatest([
-      toObservable(this.showService.showsWatched.s, { injector: this.injector }),
-      toObservable(this.showService.showsProgress.s, { injector: this.injector }),
-      toObservable(this.episodeService.showsEpisodes.s, { injector: this.injector }),
-      toObservable(this.showService.showsHidden.s, { injector: this.injector }),
-      toObservable(this.tmdbService.tmdbShows.s, { injector: this.injector }),
-    ]).pipe(
-      map(([showsWatched, showsProgress, showsEpisodes, showsHidden, tmdbShows]) => {
-        const showsHiddenIds = showsHidden?.map((showHidden) => showHidden.show.ids.trakt) ?? [];
+  getEpisodeStats(): Signal<EpisodeStats> {
+    return computed(() => {
+      const showsProgress = this.showService.showsProgress.s();
+      const showsHidden = this.showService.showsHidden.s();
 
-        // [showEnded, showWithNextEpisode]
-        const showsStats: [boolean, boolean][] =
-          showsWatched?.map((showWatched) => {
-            const isShowHidden = showsHiddenIds.includes(showWatched.show.ids.trakt);
-            if (isShowHidden) return [true, false];
+      const allShowsProgress = Object.values(showsProgress);
+      const showsNotHiddenProgress = this.getShowsNotHiddenProgress(showsProgress, showsHidden);
 
-            const showProgress = showsProgress[showWatched.show.ids.trakt];
-            if (!showProgress) return [true, false];
+      const showsEpisodesCounts = allShowsProgress.map((progress) =>
+        this.getEpisodeCount(progress),
+      );
+      const showsNotHiddenEpisodesCounts = showsNotHiddenProgress.map((progress) =>
+        this.getEpisodeCount(progress),
+      );
+      const showsWatchedEpisodesCounts = allShowsProgress.map((progress) => {
+        if (!progress) return 0;
+        const watchedProgress = this.getWatchedEpisodes(progress);
+        return this.getEpisodeCount(watchedProgress);
+      });
+      const showsWatchedNotHiddenEpisodesCounts = showsNotHiddenProgress.map((progress) => {
+        if (!progress) return 0;
+        const watchedProgress = this.getWatchedEpisodes(progress);
+        return this.getEpisodeCount(watchedProgress);
+      });
 
-            const nextEpisode = showProgress.next_episode;
-            const nextEpisodeFull =
-              nextEpisode &&
-              showsEpisodes[
-                episodeId(showWatched.show.ids.trakt, nextEpisode.season, nextEpisode.number)
-              ];
-            const withNextEpisode =
-              !!nextEpisodeFull?.first_aired && new Date(nextEpisodeFull.first_aired) < new Date();
+      return {
+        episodesCount: sum(showsEpisodesCounts),
+        watchedEpisodesCount: sum(showsWatchedEpisodesCounts),
+        notHiddenEpisodesCount: sum(showsNotHiddenEpisodesCounts),
+        notHiddenWatchedEpisodesCount: sum(showsWatchedNotHiddenEpisodesCounts),
+      };
+    });
+  }
 
-            const tmdbShow = showWatched.show.ids.tmdb
-              ? tmdbShows[showWatched.show.ids.tmdb]
-              : undefined;
-            if (!tmdbShow) return [true, false];
+  getShowStats(): Signal<ShowStats> {
+    return computed(() => {
+      const showsWatched = this.showService.showsWatched.s();
+      const showsProgress = this.showService.showsProgress.s();
+      const showsEpisodes = this.episodeService.showsEpisodes.s();
+      const showsHidden = this.showService.showsHidden.s();
+      const tmdbShows = this.tmdbService.tmdbShows.s();
 
-            return [isShowEnded(tmdbShow), withNextEpisode];
-          }) ?? [];
+      const showsHiddenIds = showsHidden?.map((showHidden) => showHidden.show.ids.trakt) ?? [];
 
-        const showsEnded = showsStats.map((showStats) => showStats[0]);
-        const showsWithNextEpisode = showsStats.map((showStats) => showStats[1]);
+      // [showEnded, showWithNextEpisode]
+      const showsStats: [boolean, boolean][] =
+        showsWatched?.map((showWatched) => {
+          const isShowHidden = showsHiddenIds.includes(showWatched.show.ids.trakt);
+          if (isShowHidden) return [true, false];
 
-        const showsCount = showsWatched?.length ?? 0;
-        const showsEndedCount = sumBoolean(showsEnded);
-        const showsReturningCount = showsCount - showsEndedCount;
-        const showsWithNextEpisodeCount = sumBoolean(showsWithNextEpisode);
+          const showProgress = showsProgress[showWatched.show.ids.trakt];
+          if (!showProgress) return [true, false];
 
-        return {
-          showsCount,
-          showsEndedCount,
-          showsReturningCount,
-          showsWithNextEpisodeCount,
-        };
-      }),
-    );
+          const nextEpisode = showProgress.next_episode;
+          const nextEpisodeFull =
+            nextEpisode &&
+            showsEpisodes[
+              episodeId(showWatched.show.ids.trakt, nextEpisode.season, nextEpisode.number)
+            ];
+          const withNextEpisode =
+            !!nextEpisodeFull?.first_aired && new Date(nextEpisodeFull.first_aired) < new Date();
 
-    const episodeStats: Observable<EpisodeStats> = combineLatest([
-      toObservable(this.showService.showsProgress.s, { injector: this.injector }),
-      toObservable(this.showService.showsHidden.s, { injector: this.injector }),
-    ]).pipe(
-      map(([showsProgress, showsHidden]) => {
-        const allShowsProgress = Object.values(showsProgress);
-        const showsNotHiddenProgress = this.getShowsNotHiddenProgress(showsProgress, showsHidden);
+          const tmdbShow = showWatched.show.ids.tmdb
+            ? tmdbShows[showWatched.show.ids.tmdb]
+            : undefined;
+          if (!tmdbShow) return [true, false];
 
-        const showsEpisodesCounts = allShowsProgress.map((progress) =>
-          this.getEpisodeCount(progress),
-        );
-        const showsNotHiddenEpisodesCounts = showsNotHiddenProgress.map((progress) =>
-          this.getEpisodeCount(progress),
-        );
-        const showsWatchedEpisodesCounts = allShowsProgress.map((progress) => {
-          if (!progress) return 0;
-          const watchedProgress = this.getWatchedEpisodes(progress);
-          return this.getEpisodeCount(watchedProgress);
-        });
-        const showsWatchedNotHiddenEpisodesCounts = showsNotHiddenProgress.map((progress) => {
-          if (!progress) return 0;
-          const watchedProgress = this.getWatchedEpisodes(progress);
-          return this.getEpisodeCount(watchedProgress);
-        });
+          return [isShowEnded(tmdbShow), withNextEpisode];
+        }) ?? [];
 
-        return {
-          episodesCount: sum(showsEpisodesCounts),
-          watchedEpisodesCount: sum(showsWatchedEpisodesCounts),
-          notHiddenEpisodesCount: sum(showsNotHiddenEpisodesCounts),
-          notHiddenWatchedEpisodesCount: sum(showsWatchedNotHiddenEpisodesCounts),
-        };
-      }),
-    );
+      const showsEnded = showsStats.map((showStats) => showStats[0]);
+      const showsWithNextEpisode = showsStats.map((showStats) => showStats[1]);
 
-    return combineLatest([showStats, episodeStats]);
+      const showsCount = showsWatched?.length ?? 0;
+      const showsEndedCount = sumBoolean(showsEnded);
+      const showsReturningCount = showsCount - showsEndedCount;
+      const showsWithNextEpisodeCount = sumBoolean(showsWithNextEpisode);
+
+      return {
+        showsCount,
+        showsEndedCount,
+        showsReturningCount,
+        showsWithNextEpisodeCount,
+      };
+    });
   }
 
   getShowsNotHiddenProgress(

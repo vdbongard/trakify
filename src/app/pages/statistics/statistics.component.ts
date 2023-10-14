07355b5ac5d77d5d2439221c08b31881280a009e
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2023-10-14 19:03:19 +0200

    refactor: use computed instead of combineLatest

--- src/app/pages/statistics/statistics.component.ts
+++ src/app/pages/statistics/statistics.component.ts
@@ -4,11 +4,9 @@ import { StatsService } from './data/stats.service';
 import { onError } from '@helper/error';
 import { LoadingState } from '@type/Enum';
 import type { Stats } from '@type/Trakt';
-import type { EpisodeStats, ShowStats } from '@type/Stats';
 import { LoadingComponent } from '@shared/components/loading/loading.component';
 import { CommonModule } from '@angular/common';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { minutesToDays } from '@helper/minutesToDays';
 
 @Component({
@@ -22,30 +20,19 @@ export default class StatisticsComponent {
   statsService = inject(StatsService);
   snackBar = inject(MatSnackBar);
 
-  pageState = signal(LoadingState.LOADING);
-  stats = signal<Stats | undefined>(undefined);
-  episodeStats?: EpisodeStats;
-  showStats?: ShowStats;
-  daysWatched = computed(() => minutesToDays(this.stats()?.episodes.minutes ?? 0));
+  showStats = this.statsService.getShowStats();
+  episodeStats = this.statsService.getEpisodeStats();
+  apiStatsState = signal(LoadingState.LOADING);
+  apiStats = signal<Stats | undefined>(undefined);
+  daysWatched = computed(() => minutesToDays(this.apiStats()?.episodes.minutes ?? 0));
 
   constructor() {
-    this.statsService
-      .getStats$()
-      .pipe(takeUntilDestroyed())
-      .subscribe({
-        next: ([showStats, episodeStats]) => {
-          this.episodeStats = episodeStats;
-          this.showStats = showStats;
-        },
-        error: (error) => onError(error, this.snackBar, [this.pageState]),
-      });
-
     this.statsService.fetchStats().subscribe({
       next: (stats) => {
-        this.stats.set(stats);
-        this.pageState.set(LoadingState.SUCCESS);
+        this.apiStats.set(stats);
+        this.apiStatsState.set(LoadingState.SUCCESS);
       },
-      error: (error) => onError(error, this.snackBar, [this.pageState]),
+      error: (error) => onError(error, this.snackBar, [this.apiStatsState]),
     });
   }
 }

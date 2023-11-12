d3c1b463c6a8c6378f90465ac3f81dc8d2b86dfb
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2023-11-12 18:55:57 +0100

    refactor: use same file structure as new angular project

---
+++ src/app/app.config.ts
@@ -0,0 +1,42 @@
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
+import { provideRouter, withInMemoryScrolling, withViewTransitions } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import { apiAuthInterceptor } from '@shared/interceptors/api-auth.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { OAuthStorage, provideOAuthClient } from 'angular-oauth2-oidc';
+import { provideServiceWorker } from '@angular/service-worker';
+import { firebaseProviders } from '../firebase.providers';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(
+      routes,
+      withInMemoryScrolling({ scrollPositionRestoration: 'disabled' }),
+      withViewTransitions(),
+    ),
+    provideHttpClient(withInterceptors([apiAuthInterceptor]), withInterceptorsFromDi()),
+    provideAnimations(),
+    provideOAuthClient({
+      resourceServer: {
+        allowedUrls: ['https://api.trakt.tv'],
+        sendAccessToken: true,
+      },
+    }),
+    {
+      provide: OAuthStorage,
+      useFactory: (): OAuthStorage => localStorage,
+    },
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    firebaseProviders,
+    importProvidersFrom(MatSnackBarModule, MatDialogModule),
+  ],
+};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,40 +1,40 @@
 import { Routes } from '@angular/router';
 import { path } from '@helper/path';
 import { about, lists, login, redirect, shows, statistics } from '@shared/paths';
+import showRoutes from './pages/shows/routes';
 import { loggedIn } from '@shared/guards/logged-in';
-import showRoutes from './app/pages/shows/routes';
 
 export const routes: Routes = [
   { path: '', redirectTo: path(shows), pathMatch: 'full' },
   {
     path: path(login),
-    loadComponent: () => import('./app/pages/login/login.component'),
+    loadComponent: () => import('./pages/login/login.component'),
     title: 'Login - Trakify',
   },
   {
     path: path(redirect),
-    loadComponent: () => import('./app/pages/redirect/redirect.component'),
+    loadComponent: () => import('./pages/redirect/redirect.component'),
   },
   ...showRoutes,
   {
     path: path(lists),
-    loadComponent: () => import('./app/pages/lists/lists.component'),
+    loadComponent: () => import('./pages/lists/lists.component'),
     canActivate: [loggedIn],
   },
   {
     path: path(statistics),
-    loadComponent: () => import('./app/pages/statistics/statistics.component'),
+    loadComponent: () => import('./pages/statistics/statistics.component'),
     title: 'Statistics - Trakify',
     canActivate: [loggedIn],
   },
   {
     path: path(about),
-    loadComponent: () => import('./app/pages/about/about.component'),
+    loadComponent: () => import('./pages/about/about.component'),
     title: 'About - Trakify',
   },
   {
     path: '**',
-    loadComponent: () => import('./app/pages/error/error.component'),
+    loadComponent: () => import('./pages/error/error.component'),
     title: 'Oops - Trakify',
   },
 ];

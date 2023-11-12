83716fba80a4ed427345156c18d4f2c4f95b09cb
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2023-11-12 18:55:57 +0100

    refactor: use same file structure as new angular project

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,5 @@
-import { importProvidersFrom, isDevMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { OAuthStorage, provideOAuthClient } from 'angular-oauth2-oidc';
-import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
-import { provideRouter, withInMemoryScrolling, withViewTransitions } from '@angular/router';
-import { provideServiceWorker } from '@angular/service-worker';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatDialogModule } from '@angular/material/dialog';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { routes } from './routes';
-import { apiAuthInterceptor } from '@shared/interceptors/api-auth.interceptor';
-import { firebaseProviders } from './firebase.providers';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(
-      routes,
-      withInMemoryScrolling({ scrollPositionRestoration: 'disabled' }),
-      withViewTransitions(),
-    ),
-    provideHttpClient(withInterceptors([apiAuthInterceptor]), withInterceptorsFromDi()),
-    provideAnimations(),
-    provideOAuthClient({
-      resourceServer: {
-        allowedUrls: ['https://api.trakt.tv'],
-        sendAccessToken: true,
-      },
-    }),
-    {
-      provide: OAuthStorage,
-      useFactory: (): OAuthStorage => localStorage,
-    },
-    provideServiceWorker('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000',
-    }),
-    firebaseProviders,
-    importProvidersFrom(MatSnackBarModule, MatDialogModule),
-  ],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));

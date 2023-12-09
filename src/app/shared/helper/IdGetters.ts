db03332e03b5256e5643200949ca3c6288644173
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2023-12-09 14:41:51 +0100

    fix: show poster view transition not working when show starts with number

--- src/app/shared/helper/IdGetters.ts
+++ src/app/shared/helper/IdGetters.ts
@@ -1,12 +1,12 @@
 import { EpisodeFull, Show } from '@type/Trakt';
 
 export function getShowId(show: Show | null | undefined): string {
-  return '' + (show?.ids.slug ?? '');
+  return 'show-' + (show?.ids.slug ?? '');
 }
 
 export function getShowWithEpisodeId(
   show: Show | null | undefined,
   episode: EpisodeFull | null | undefined,
 ): string {
-  return '' + (show?.ids.slug ?? '') + (episode?.ids.trakt ?? '');
+  return 'show-' + (show?.ids.slug ?? '') + (episode?.ids.trakt ?? '');
 }

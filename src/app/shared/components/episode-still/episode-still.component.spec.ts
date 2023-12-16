1b2306537c0a90715c6ee0e8b2693663a9dfae54
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2023-12-16 23:11:01 +0100

    refactor: extract episode-still component

---
+++ src/app/shared/components/episode-still/episode-still.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EpisodeStillComponent } from './episode-still.component';
+
+describe('EpisodeStillComponent', () => {
+  let component: EpisodeStillComponent;
+  let fixture: ComponentFixture<EpisodeStillComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [EpisodeStillComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(EpisodeStillComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

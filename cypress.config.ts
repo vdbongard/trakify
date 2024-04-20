e9364bd4ffeb2f60977855d87fe54e4e37bad058
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2024-04-20 21:27:47 +0200

    fix: error in cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,6 @@ export default defineConfig({
   defaultCommandTimeout: 8000,
   e2e: {
     baseUrl: 'http://localhost:4200/',
-    experimentalSessionAndOrigin: true,
     setupNodeEvents(on, config): PluginConfigOptions | void {
       if (config.isTextTerminal) {
         return { ...config, excludeSpecPattern: ['cypress/e2e/$all.cy.ts'] };

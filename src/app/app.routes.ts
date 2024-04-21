9cc3aa66e56239ff3a2bb11a384a269477adb88f
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2024-04-21 17:54:23 +0200

    refactor: use sfc for statistics page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -23,7 +23,7 @@ export const routes: Routes = [
   },
   {
     path: path(statistics),
-    loadComponent: () => import('./pages/statistics/statistics.component'),
+    loadComponent: () => import('./pages/statistics/statistics.analog'),
     title: 'Statistics - Trakify',
     canActivate: [loggedIn],
   },

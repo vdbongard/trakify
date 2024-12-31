144b4251bb935bbe6d3cc5c595d4c30cc87000dc
Daniel van den Bongard <d.van-den-bongard@greyrook.com>
2024-12-31 21:20:47 +0100

    fix: lagging ticker animation

--- src/app/shared/components/ticker/ticker.component.ts
+++ src/app/shared/components/ticker/ticker.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, inject, input } from '@angular/core';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
@@ -11,30 +11,39 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
     // eslint-disable-next-line @typescript-eslint/naming-convention
     '[class.ticker]': 'tickerIf()',
     // eslint-disable-next-line @typescript-eslint/naming-convention
-    '[style.--indent.px]': 'indent',
+    '[style.--animated-text-width]': 'animatedTextWidth',
     // eslint-disable-next-line @typescript-eslint/naming-convention
-    '[style.transitionDuration.ms]': 'duration',
+    '(mouseenter)': 'onMouseEnter()',
     // eslint-disable-next-line @typescript-eslint/naming-convention
-    '(mouseenter)': 'onMouseEnter($event.target)',
+    '(mouseleave)': 'onMouseLeave()',
   },
 })
 export class TickerComponent {
+  element = inject(ElementRef);
+
   tickerIf = input(true);
 
-  indent = 0;
+  animatedTextWidth = 0;
 
-  get duration(): number {
-    return -12 * this.indent;
+  onMouseEnter(): void {
+    this.calculateAnimatedTextWidth();
+    this.element.nativeElement.style.setProperty('overflow', 'visible');
   }
 
-  onMouseEnter({ scrollWidth, clientWidth }: HTMLElement): void {
-    const hasOverflow = scrollWidth > clientWidth;
+  onMouseLeave(): void {
+    this.element.nativeElement.style.setProperty('overflow', 'hidden');
+  }
+
+  calculateAnimatedTextWidth(): void {
+    const label = this.element.nativeElement;
+    const hasOverflow = label.scrollWidth > label.clientWidth;
 
     if (!hasOverflow) {
+      this.animatedTextWidth = 0;
       return;
     }
 
-    const offset = 1; // needed for end of text to be visible on hover
-    this.indent = clientWidth - scrollWidth - offset;
+    const offset = 1; // Needed for end of text to be visible on hover
+    this.animatedTextWidth = label.scrollWidth - label.clientWidth + offset;
   }
 }
